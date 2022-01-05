2744594b160828199105e2c8024f52cb0166b789
DESKTOP-EP5LOO7\nguye <huannv.bhsoft@gmail.com>
2022-01-05 22:18:14 +0700

    test api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+const App: React.FC = () => {
+  const [data, setData] = React.useState<any[]>();
+  console.log(data);
+  React.useEffect(() => {
+    fetch("https://www.kiotmpec.xyz/api/v1/app/public/khach-hang/search")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.data.currentElements[0].diaChi);
+        setData([...data.data.currentElements]);
+      });
+  }, []);
   return (
     <div className="App">
-     
+      <span>
+        {data?.map((item) => {
+          return <div>{item.id}</div>;
+        })}
+      </span>
     </div>
   );
-}
+};
 
 export default App;
